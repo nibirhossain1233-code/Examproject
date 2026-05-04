a2264c3a09383e1411f31bc8f0cbeabf1bec3048
Nibir Hossain <nibirhossain1233@gmail.com>
2026-05-04 06:43:32 +0600

    Added Services section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/home/Banner";
 import Experience from "../components/home/Experience";
+import Services from "../components/home/Services";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Experience />
+      <Services />
     </>
   );
 };
